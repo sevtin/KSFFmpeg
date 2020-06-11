5b1fca99b1e38108d70a4537bb9736942f5aecaf
saeipi <ksert@163.com>
2020-06-11 15:44:51 +0800

    错误修正

--- FFmpegExamples/scaling_video/KSMediaPlayer/FFmpeg/scaling_video.c
+++ FFmpegExamples/scaling_video/KSMediaPlayer/FFmpeg/scaling_video.c
@@ -24,7 +24,7 @@ static void fill_yuv_image(uint8_t *data[4], int linesize[4], int width, int hei
     /* Cb and Cr */
     for (y = 0; y <height/2; y++) {
         for (x = 0; x < width/2; x++) {
-            data[1][y * linesize[2] + x] = 128 + y + frame_index * 2;
+            data[1][y * linesize[1] + x] = 128 + y + frame_index * 2;
             data[2][y * linesize[2] + x] = 64 + x +frame_index * 5;
         }
     }
@@ -34,7 +34,7 @@ int scaling_video_port(char *dst_url, char *format) {
     uint8_t *src_data[4], *dst_data[4];
     int src_linesize[4], dst_linesize[4];
     int src_w = 320, src_h = 240, dst_w, dst_h;
-    enum AVPixelFormat src_pix_fmt = AV_PIX_FMT_YUV420P, dst_pix_fmt = AV_PIX_FMT_YUV420P;
+    enum AVPixelFormat src_pix_fmt = AV_PIX_FMT_YUV420P, dst_pix_fmt = AV_PIX_FMT_RGB24;
     const char *dst_size = NULL;
     const char *dst_filename = NULL;
     FILE *dst_file;
