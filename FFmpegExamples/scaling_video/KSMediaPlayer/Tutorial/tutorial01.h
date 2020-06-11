d272bb02b38de2d9e6565a51907c80240800d32d
saeipi <ksert@163.com>
2020-06-11 18:57:17 +0800

    tutorial01 原版

---
+++ FFmpegExamples/scaling_video/KSMediaPlayer/Tutorial/tutorial01.h
@@ -0,0 +1,14 @@
+//
+//  tutorial01.h
+//  KSMediaPlayer
+//
+//  Created by saeipi on 2020/6/11.
+//  Copyright © 2020 saeipi. All rights reserved.
+//
+
+#ifndef tutorial01_h
+#define tutorial01_h
+
+#include <stdio.h>
+
+#endif /* tutorial01_h */
